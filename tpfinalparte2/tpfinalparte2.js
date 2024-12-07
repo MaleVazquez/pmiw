fd868de813eb76973c078badbd6f23178d2dbf03
MaleVazquez <129406271+MaleVazquez@users.noreply.github.com>
2024-12-07 01:33:04 -0300

    Add files via upload

---
+++ tpfinalparte2/tpfinalparte2.js
@@ -0,0 +1,37 @@
+//Leonela Malena Vazquez
+//Leandro Nicolas Vazquez
+
+//LINK AL VIDEO DE MALENA: https://www.youtube.com/watch?v=o-jRI-k85hk
+
+
+
+let juego;
+
+
+function preload() {
+  miAudio = loadSound('assets/again.mp3');
+}
+
+function setup() {
+  createCanvas(650, 480);
+  juego = new Juego();
+  iniciarJuego();
+   miAudio.loop();
+}
+
+function draw() {
+  juego.dibujar();
+}
+
+function keyPressed() {
+  juego.keyPressed();
+  audio.tecla(key);
+}
+
+function keyReleased() {
+  juego.keyReleased();
+}
+
+function iniciarJuego() {
+  frameRate(60);
+}
